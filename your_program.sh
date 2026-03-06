81b2cd5741fb37a03168cd957d00fe6e63d1b498
ratnajagadeesharava <ratnajagadeesh.arava@outlook.in>
2026-03-06 18:02:19 +0000

    codecrafters submit [skip ci]

--- your_program.sh
+++ your_program.sh
@@ -22,8 +22,8 @@ set -e # Exit early if any commands fail
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
 # exec /tmp/codecrafters-build-redis-rust/release/codecrafters-redis "$@"
-printf "*1\r\n$4\r\nPING\r\n" | nc 127.0.0.1 6379
-printf '*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n' | nc 127.0.0.1 6379
-printf '*5\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n$2\r\nPX\r\n$4\r\n5000\r\n' | nc 127.0.0.1 6379
-printf '*5\r\n$3\r\nSET\r\n$6\r\norange\r\n$5\r\ngrape\r\n$2\r\nPX\r\n$3\r\n100\r\n' | nc 127.0.0.1 6379
+# printf "*1\r\n$4\r\nPING\r\n" | nc 127.0.0.1 6379
+# printf '*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n' | nc 127.0.0.1 6379
+# printf '*5\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n$2\r\nPX\r\n$4\r\n5000\r\n' | nc 127.0.0.1 6379
+# printf '*5\r\n$3\r\nSET\r\n$6\r\norange\r\n$5\r\ngrape\r\n$2\r\nPX\r\n$3\r\n100\r\n' | nc 127.0.0.1 6379
 printf '*2\r\n$4\r\nECHO\r\n$4\r\npear\r\n' | nc 127.0.0.1 6379
