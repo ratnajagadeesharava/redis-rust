4b616529e7122c66da24cccd77c6c5ea6c6dccfa
ratnajagadeesharava <ratnajagadeesh.arava@outlook.in>
2026-03-09 16:47:17 +0000

    codecrafters submit [skip ci]

--- your_program.sh
+++ your_program.sh
@@ -30,4 +30,4 @@ set -e # Exit early if any commands fail
 # printf '*4\r\n$5\r\nRPUSH\r\n$9\r\nraspberry\r\n$6\r\nbanana\r\n$4\r\npear\r\n' | nc 127.0.0.1 6379
 # printf '*3\r\n$5\r\nBLPOP\r\n$6\r\norange\r\n$1\r\n0\r\n' | nc 
 # printf '*4\r\n$6\r\nLRANGE\r\n$6\r\nbanana\r\n$1\r\n0\r\n$2\r\n-1\r\n' | nc 127.0.0.1 6379
-printf '*5\r\n$4\r\nXADD\r\n$6\r\nbanana\r\n$3\r\n1-*\r\n$9\r\npineapple\r\n$6\r\norange\r\n' | nc 127.0.0.1 6379
+printf '*5\r\n$4\r\nXADD\r\n$10\r\nstrawberry\r\n$1\r\n*\r\n$3\r\nfoo\r\n$3\r\nbar\r\n' | nc 127.0.0.1 6379
